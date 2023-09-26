84bc71332e7d039a8782edca355db4347078d143
gatwirival <valegjohn@gmail.com>
2023-09-26 12:56:05 +0300

    prismane styling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import { Box } from "@prismane/core";
 
-function HomePage() {
+const Page = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <Box bg={["diamond", 300]}>Hello, world</Box>
+      {/* Prismane Color */}
+      <Box bg={["primary", 300]}>Hello, world</Box> {/* Theme Color */}
+      <Box
+        bg={(theme) => (theme.mode === "dark" ? ["red", 300] : ["red", 600])} // With Theme Object
+      >
+        Hello, world
+      </Box>
+      <Box bg={[["primary", 600], { hover: ["base", 700] }]}>Hello, world</Box>
+    </>
   );
-}
+};
 
-export default HomePage;
+export default Page;
