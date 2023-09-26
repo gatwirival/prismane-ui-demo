0d559afca6c437df37ea9b2ee1b65bdb8feff177
gatwirival <valegjohn@gmail.com>
2023-09-26 13:40:28 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,42 @@
-import { Box } from "@prismane/core";
+import { Card, Image, Text, Button, Flex, fr } from "@prismane/core";
+import { Star, ShoppingCart } from "@phosphor-icons/react";
 
-const Page = () => {
+
+export default function App() {
   return (
-    <>
-      <Box bg={["diamond", 300]}>Hello, world</Box>
-      {/* Prismane Color */}
-      <Box bg={["primary", 300]}>Hello, world</Box> {/* Theme Color */}
-      <Box
-        bg={(theme) => (theme.mode === "dark" ? ["red", 300] : ["red", 600])} // With Theme Object
-      >
-        Hello, world
-      </Box>
-      <Box bg={[["primary", 600], { hover: ["base", 700] }]}>Hello, world</Box>
-    </>
+    <Card w={360} h={540} gap={fr(2)}>
+      <Image
+        src="https://img.freepik.com/free-photo/black-headphones-digital-device_53876-96805.jpg?size=626&ext=jpg&ga=GA1.1.460882575.1681882906&semt=sph"
+        br="md"
+        fit="cover"
+        mb={fr(2)}
+      />
+      <Flex gap={fr(2)}>
+        <Text fs="md" cl="green">
+          <Star /> 4.5 (120 reviews)
+        </Text>
+        <Text fs="md" cl="blue">
+          In Stock
+        </Text>
+      </Flex>
+      <Text fs="lg" fw="bold" cl="black">
+        Premium Headphones
+      </Text>
+      <Text cl="gray">
+        Enjoy crystal-clear sound quality with our premium headphones, perfect
+        for music lovers and audiophiles.
+      </Text>
+      <Text fw="bold" fs="2xl" cl="primary">
+        $149.99
+      </Text>
+      <Flex gap={fr(4)} mt="auto">
+        <Button cl="primary" bg="yellow" hoverBg="orange">
+          Add to Wishlist
+        </Button>
+        <Button cl="white" bg="blue" hoverBg="darkblue">
+          <ShoppingCart /> Add to Cart
+        </Button>
+      </Flex>
+    </Card>
   );
-};
-
-export default Page;
+}
