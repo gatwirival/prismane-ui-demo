060752b2a549b61715f048af5ff789322be9ad8d
gatwirival <valegjohn@gmail.com>
2023-09-26 10:15:38 +0300

    first commit

---
+++ pages/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@prismane/core/themes';
+
+const theme = createTheme({
+  colors: {
+    primary: {
+      100: '#0070f3', // Replace with your desired color
+    },
+  },
+});
+
+export default theme;
