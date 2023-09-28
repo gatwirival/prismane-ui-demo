5b4deca50d72c28a50245874e111c3679ba93060
gatwirival <valegjohn@gmail.com>
2023-09-28 12:41:22 +0300

    styling retification

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import { Card, Image, Text, Button, Flex, fr } from "@prismane/core";
+import { Card, Image, Text, Button, Flex, fr, PrismaneProvider } from "@prismane/core";
 import { Star, ShoppingCart } from "@phosphor-icons/react";
 
 
 export default function App() {
   return (
-    <Card w={360} h={540} gap={fr(2)}>
+    <PrismaneProvider>
+    <Card w={360} gap={fr(2)}>
       <Image
         src="https://img.freepik.com/free-photo/black-headphones-digital-device_53876-96805.jpg?size=626&ext=jpg&ga=GA1.1.460882575.1681882906&semt=sph"
         br="md"
@@ -30,13 +31,14 @@ export default function App() {
         $149.99
       </Text>
       <Flex gap={fr(4)} mt="auto">
-        <Button cl="primary" bg="yellow" hoverBg="orange">
+        <Button cl="primary" bg="yellow">
           Add to Wishlist
         </Button>
-        <Button cl="white" bg="blue" hoverBg="darkblue">
+        <Button cl="white" bg="blue">
           <ShoppingCart /> Add to Cart
         </Button>
       </Flex>
     </Card>
+    </PrismaneProvider>
   );
 }
